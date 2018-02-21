bf47d6105dc8a77ec27133fe23380eea75b72391
shaswat-indian <33908100+shaswat-indian@users.noreply.github.com>
2018-02-21 19:46:01 +0530

    Basic Files Program-3
    Appending data to a File.

---
+++ p3/p3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void main()
+{
+	FILE *f1;
+	char fname[80],contents[80];
+	printf("Enter Filename: ");
+	fgets(fname,80,stdin);
+	f1=fopen(fname,"a");
+	//fgets("%s",fname);
+	printf("\nEnter Contents to Append to file:-\n");
+	while(fgets(contents,80,stdin))
+		fputs(contents,f1);
+	
+	//fputc('\n',f1);
+	fclose(f1);
+	
+	printf("\n\nFile Contents are:-\n");
+	f1=fopen(fname,"r");
+	while(fgets(contents,80,f1))
+		fputs(contents,stdout);
+
+	fclose(f1);
+
+}
