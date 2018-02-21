c83d38f5a5590d3146a2ad8a277d242af189baf1
shaswat-indian <33908100+shaswat-indian@users.noreply.github.com>
2018-02-21 19:34:45 +0530

    Basic Files Program-1
    Writing to a File and Reading from it.

---
+++ p1/p1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void main()
+{
+	FILE *f1;
+	char fname[80],contents[80];
+	printf("Enter Filename: ");
+	fgets(fname,80,stdin);
+	f1=fopen(fname,"w");
+	//fgets("%s",fname);
+	printf("\nEnter Contents to write to file:-\n");
+	while(fgets(contents,80,stdin))
+		fputs(contents,f1);
+	
+	//fputc('\n',f1);
+	fclose(f1);
+	
+	printf("\n\nFile Contents are:-\n");
+	f1=fopen(fname,"r");
+	while(fgets(contents,80,f1))
+		fputs(contents,stdout);
+
+	fclose(f1);
+
+}
